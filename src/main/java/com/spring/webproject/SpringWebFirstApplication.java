d11dd00457e990c87deeface98d053fbf04b307a
Ram <mbramakrishna12@gmail.com>
2022-01-15 17:52:47 +0530

    project all codes

---
+++ src/main/java/com/spring/webproject/SpringWebFirstApplication.java
@@ -0,0 +1,13 @@
+package com.spring.webproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/webproject/SpringWebFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
